9617b84ed44f6bb555e0f095e8bbbca103a6259b
Sebastian Zarate <zaratesanmiguel2019@outlook.com>
2023-05-24 02:56:00 -0500

    Init

--- Chip8/main.cpp
+++ Chip8/main.cpp
@@ -1,25 +1,7 @@
 #include <iostream>
 #include <SDL.h>
 
-const int WIDTH = 800, HEIGHT = 600;
-
-bool init_sdl(void) {
-    bool _return = true;
-    if (SDL_Init(SDL_INIT_VIDEO | SDL_INIT_AUDIO | SDL_INIT_TIMER) != 0) {
-        SDL_Log("Failed to Init SDL systems: ", SDL_GetError());
-        _return = false;
-    }
-    return _return;
-}
-
-void final_cleanup() {
-    SDL_Quit();
-}
-
 int main(int argc, char* argv[]) {
-    //Initialize SDL
-    if (!init_sdl()) exit(EXIT_FAILURE);
-
-    final_cleanup();
-    exit(EXIT_SUCCESS);
+    printf("Hola Mundo!");
+    return 0;
 }
